d20083eb76e57a6f983b985f05bdadad1f414a2b
evamasdareva <114887945+evamasdareva@users.noreply.github.com>
2022-12-23 13:43:06 +0700

    Add files via upload

---
+++ pertemuan2/tugaspraktikum2/average.py
@@ -0,0 +1,10 @@
+#input
+a = float(input("Masukkan banyaknya angka: "))
+b = float(input("Masukkan angka: "))
+c = float(input("Masukkan angka: "))
+d = float(input("Masukkan angka: "))
+e = float(input("Masukkan angka: "))
+f = float(input("Masukkan angka: "))
+
+hasil = (10 + 12.5 + (-4.2) + 123.9 + (-3)) / 5
+print("Hasil rata-rata perhitungan nilai adalah: " +str(hasil))
---
+++ pertemuan2/tugaspraktikum2/persegi.py
@@ -0,0 +1,7 @@
+j = int(input("Masukkan input: "))
+x = str("#")
+
+for i in range(0,j):
+    for j in range(0,10):
+        print(x,end="")
+    print()
